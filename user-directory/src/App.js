870f6a81405f0e80cc59c72033356bba66b9c935
DerpHead <alexraia4@gmail.com>
2021-04-22 18:46:47 -0400

    derp

---
+++ user-directory/src/App.js
@@ -0,0 +1,23 @@
+import './reset.css';
+import './index.css';
+import { Component } from 'react';
+import Top from './components/Top.js'
+import Slides from './components/Slides.js';
+import Bottom from './components/Bottom.js';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <Top />
+        <div className = "content">
+          <Slides />
+          <Bottom />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ user-directory/src/components/Slides.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Slides extends Component {
+
+    render () {
+        return (
+            <div>
+                <p> slide portion </p>
+            </div> 
+        )
+    }
+}
